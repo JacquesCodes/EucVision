7d0d73c1b0b9aed6b10fd063d5109261c7afedd9
JacquesCodes <144622338+JacquesCodes@users.noreply.github.com>
2025-11-16 17:19:48 +0200

    Improved RAM and storage!

--- EucVision LAScatalog.R
+++ EucVision LAScatalog.R
@@ -8,15 +8,19 @@ library(geometry)
 library(dplyr)
 library(future)
 
-# DO NOT USE LAZ.!! ONLY USE LAS. IT IMPROVES PERFORMANCE X10!
+# Read in all point clouds and shape files ####
 
-setwd("E:/Remote Sensing Media/0. R Projects/3. 7 November 2025")
+# My path to the remote sensing dataset
+myPath <- "E:/Remote Sensing Media/08. 30 October & 7 November 2025/"
 
-ctg <- readLAScatalog("E:/Remote Sensing Media/8. 7 November 2025/Point cloud")
+# Read in point clouds into a catalog (ctg)
+ctg <- readLAScatalog(paste0(myPath,"03. Point clouds"))
 
-plots_buffered_unsorted <- st_read(paste0("E:/Remote Sensing Media/1. QGIS Projects/1. Plot boundaries for cropping/R Plots.shp"))
+# Read in shape files for points of interest boundaries like my plots
+plots_buffered_unsorted <- st_read(paste0("E:/Remote Sensing Media/00. QGIS Projects/1. Plot boundaries for cropping/R Plots.shp"))
 plots <- plots_buffered_unsorted[order(plots_buffered_unsorted$id), ]
 
+# Read in tree location for height extraction at the end
 trees <- st_read("C:/Users/jakev/Stellenbosch University/JacquesV B.Sc. skripsie M.Sc. project - Documents/Processed Data/EucVision/QGIS Combined Output/All_Plots.shp")
 
 # View catalog, plots and trees
@@ -36,13 +40,10 @@ opt_select(ctg) <- "xyz"
 # tic() & toc() is to check code running time
 tic()
 # Write to disk rather than memory:
-opt_output_files(ctg) <- paste0(getwd(),"/1. Clipped/", "Plot_{ID}")
+opt_output_files(ctg) <- paste0(myPath,"04. Clipped/", "Plot_{ID}")
 # Crop plots
 ctg_clipped <- clip_roi(ctg, plots)
 toc()
-
-plot(ctg_clipped)
-
 
 # Classify plots ####
 
@@ -52,7 +53,7 @@ opt_select(ctg_clipped) <- "xyz"
 
 tic()
 # Write to disk rather than memory:
-opt_output_files(ctg_clipped) <- paste0(getwd(),"/2. Ground Classified/", "{*}_classified")
+opt_output_files(ctg_clipped) <- paste0(myPath,"05. Ground classified/", "{*}_classified")
 # Ground classifications :
 ctg_classified <- classify_ground(ctg_clipped, csf(sloop_smooth = TRUE, 
                                                    class_threshold = 0.01, 
@@ -60,13 +61,15 @@ ctg_classified <- classify_ground(ctg_clipped, csf(sloop_smooth = TRUE,
                                                    time_step = 1))
 toc()
 
-# Class_threshold = The distance to the simulated cloth to classify a point cloud into ground and non-ground. 
+# Class_threshold 
+# => The distance to the simulated cloth to classify a point cloud into ground and non-ground. 
 # The default is 0.5. 
 # Need to be set no larger than the smallest tree. 0.01 preferred for best height estimations.
 # The higher the value the higher the ground classifications become. 
 
 
-# Cloth_resolution = The distance between particles in the cloth. 
+# Cloth_resolution 
+# => The distance between particles in the cloth. 
 # This is usually set to the average distance of the points in the point cloud. 
 # The default value is 0.5.
 # PREFFERED = 1
@@ -85,7 +88,7 @@ opt_select(ctg_classified) <- "xyz"
 
 tic()
 # Write to disk rather than memory:
-opt_output_files(ctg_classified) <- paste0(getwd(),"/3. Normalised/", "{*}_normalised")
+opt_output_files(ctg_classified) <- paste0(myPath,"06. Normalised/", "{*}_normalised")
 # A point cloud-based normalization without a raster:
 ctg_normalised <- normalize_height(las = ctg_classified, algorithm = tin())
 toc()
@@ -99,15 +102,13 @@ toc()
 
 # Limit the amount of workers (threads) if you don't have enough RAM. Each worker uses own RAM.
 # plan(multisession)
-plan(multisession, workers = 4)
-
+plan(multisession, workers = 6)
 opt_independent_files(ctg_normalised) <- TRUE
-plot(ctg_normalised, chunk = TRUE)
 opt_select(ctg_normalised) <- "xyz"
 
 tic()
 # Write to disk rather than memory:
-opt_output_files(ctg_normalised) <- paste0(getwd(),"/4. Canopy Height Model/", "{*}_chm")
+opt_output_files(ctg_normalised) <- paste0(myPath,"07. Canopy Height Model/", "{*}_chm")
 # Rasterize canopy with interpolation:
 ctg_chm <- rasterize_canopy(ctg_normalised, res = 0.01, algorithm = p2r(na.fill = tin()))
 print("Rasterize canopy time:")
@@ -115,6 +116,7 @@ toc()
 
 # Extract trees' heights ####
 
+tic()
 # Ensure both have an ID column
 trees$ID <- 1:nrow(trees)
 
@@ -125,9 +127,9 @@ tree_heights <- terra::extract(ctg_chm, trees, fun = max, na.rm = TRUE)
 trees_with_heights <- left_join(trees, st_drop_geometry(tree_heights), by = "ID")
 
 # Save to file
-st_write(trees_with_heights, paste0(getwd(),"/5. Heights/All Plots.shp"))
-st_write(trees_with_heights, paste0(getwd(),"/5. Heights/All Plots.csv"))
-
+st_write(trees_with_heights, paste0(myPath,"08. Heights/All Plots.shp"))
+st_write(trees_with_heights, paste0(myPath,"08. Heights/All Plots.csv"))
+toc()
 
 
 
