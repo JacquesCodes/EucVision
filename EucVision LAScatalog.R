f7087f34ba6b5aad2b64b09b26412af59b7e483d
JacquesCodes <144622338+JacquesCodes@users.noreply.github.com>
2025-11-26 15:46:08 +0200

    Working Well

--- EucVision LAScatalog.R
+++ EucVision LAScatalog.R
@@ -12,17 +12,17 @@ library(sp)
 # Read in all point clouds and shape files ####
 
 # My path to the remote sensing dataset
-myPath <- "E:/Remote Sensing Media/03. Feb 2025/"
+myPath <- "E:/Remote Sensing Media/10. 17 November 2025/"
 
 # Read in point clouds into a catalog (ctg)
 ctg <- readLAScatalog(paste0(myPath,"03. Point clouds"))
 
-# Read in shape files for points of interest boundaries like my plots
+# Read in shape files for individual plot boundaries
 plots_buffered_unsorted <- st_read(paste0("E:/Remote Sensing Media/00. QGIS Projects/1. Plot boundaries for cropping/R Plots.shp"))
 plots <- plots_buffered_unsorted[order(plots_buffered_unsorted$id), ]
 
-# Read in tree location for height extraction at the end
-trees <- st_read("C:/Users/jakev/Stellenbosch University/JacquesV B.Sc. skripsie M.Sc. project - Documents/Processed Data/EucVision/QGIS Combined Output/All_Plots.shp")
+# Read in tree location for height extraction
+trees <- st_read("C:/Users/jakev/Stellenbosch University/JacquesV B.Sc. skripsie M.Sc. project - Documents/Processed Data/EucVision/QGIS Combined Output/1 September 2025/All_Plots.shp")
 
 # View catalog, plots and trees
 plot(ctg)
@@ -33,15 +33,15 @@ plot(trees$geometry, add = TRUE, col = "red")
 
 # Multiple CPU threads mode
 plan(multisession)
-# .las files overlaps and are not independent
+# .las files overlaps and are dependent
 opt_independent_files(ctg) <- FALSE
-# Only load needed variables
+# Only load needed variables into memory
 opt_select(ctg) <- "xyz"
 
 # tic() & toc() is to check code running time
 tic()
 # Write to disk rather than memory:
-opt_output_files(ctg) <- paste0(myPath,"04. Clipped/", "Plot_{ID}")
+opt_output_files(ctg) <- paste0(myPath,"04. Point clouds clipped/", "Plot_{ID}")
 # Crop plots
 ctg_clipped <- clip_roi(ctg, plots)
 toc()
@@ -54,7 +54,7 @@ opt_select(ctg_clipped) <- "xyz"
 
 tic()
 # Write to disk rather than memory:
-opt_output_files(ctg_clipped) <- paste0(myPath,"05. Ground classified/", "{*}_classified")
+opt_output_files(ctg_clipped) <- paste0(myPath,"05. Point clouds ground classified/", "{*}_classified")
 # Ground classifications :
 ctg_classified <- classify_ground(ctg_clipped, csf(sloop_smooth = TRUE, 
                                                    class_threshold = 0.01, 
@@ -76,11 +76,11 @@ toc()
 # PREFFERED = 1
 # DO NOT MAKE LOWER THAN 1 as it classify trees as ground points because of canopy closure
 # Needed to make value to 1 for 1m x 1m plots 
-# The cloth falls between the points and classify trees.
+# Otherwise the cloth falls between the points and classify trees.
 
 # Normalize plots ####
 
-# If in the future I am unable to classify ground well. I should use a baseline raster instead:
+# If in the future I user are unable to classify ground. Use a baseline raster from previous datasets:
 # ctg_normalised <- normalize_height(las = ctg_classified, algorithm = tin(), dtm = )
 
 plan(multisession)
@@ -89,7 +89,7 @@ opt_select(ctg_classified) <- "xyz"
 
 tic()
 # Write to disk rather than memory:
-opt_output_files(ctg_classified) <- paste0(myPath,"06. Normalised/", "{*}_normalised")
+opt_output_files(ctg_classified) <- paste0(myPath,"06. Point clouds normalised/", "{*}_normalised")
 # A point cloud-based normalization without a raster:
 ctg_normalised <- normalize_height(las = ctg_classified, algorithm = tin())
 toc()
@@ -103,19 +103,19 @@ toc()
 
 # Limit the amount of workers (threads) if you don't have enough RAM. Each worker uses own RAM.
 # plan(multisession)
-plan(multisession, workers = 6)
+plan(multisession, workers = 5)
 opt_independent_files(ctg_normalised) <- TRUE
 opt_select(ctg_normalised) <- "xyz"
 
 tic()
 # Write to disk rather than memory:
-opt_output_files(ctg_normalised) <- paste0(myPath,"07. Canopy Height Model/", "{*}_chm")
+opt_output_files(ctg_normalised) <- paste0(myPath,"07. Canopy Height Models/", "{*}_chm")
 # Rasterize canopy with interpolation:
 ctg_chm <- rasterize_canopy(ctg_normalised, res = 0.01, algorithm = p2r(na.fill = tin()))
 print("Rasterize canopy time:")
 toc()
 
-# Extract trees' heights ####
+# Extract tree heights ####
 
 tic()
 # Ensure both have an ID column
@@ -127,9 +127,9 @@ tree_heights <- terra::extract(ctg_chm, trees, fun = max, na.rm = TRUE)
 # Join results back using the ID
 trees_with_heights <- left_join(trees, st_drop_geometry(tree_heights), by = "ID")
 
-# Save to file
-st_write(trees_with_heights, paste0(myPath,"08. Heights/All Plots.shp"))
-st_write(trees_with_heights, paste0(myPath,"08. Heights/All Plots.csv"))
+# Save to shape and excel file
+st_write(trees_with_heights, paste0(myPath,"08. Tree heights/All Plots.shp"))
+st_write(trees_with_heights, paste0(myPath,"08. Tree heights/All Plots.csv"))
 toc()
 
 
--- LAScatalog Quality Control.R
+++ LAScatalog Quality Control.R
@@ -11,10 +11,11 @@ library(terra)
 library(rgl)
 
 # My path to the remote sensing dataset
-myPath <- "E:/Remote Sensing Media/03. Feb 2025/"
+# myPath <- "E:/Remote Sensing Media/05. 1 September 2025/"
+myPath <- "E:/Remote Sensing Media/08. 30 October & 7 November 2025/"
 
 #Plot number
-Number <- 38
+Number <- 17
 
 las <- readLAS(paste0(myPath,"04. Clipped/Plot_",Number,".las"))
 las_classified <- readLAS(paste0(myPath,"05. Ground classified/Plot_",Number, "_classified.las"))
@@ -32,27 +33,29 @@ tree_heights <- terra::extract(las_chm, PlotTrees, fun = max, na.rm = TRUE)
 # Join results back using the ID
 trees_with_heights <- left_join(PlotTrees, st_drop_geometry(tree_heights), by = "ID")
 
-# Cropped las
+# Plot cropped las
 plot(las, size = 4, bg = "white")
-# plot(las, size = 1, color = "RGB", bg = "white")
+# plot(las, size = 2, color = "RGB", bg = "white")
 
-# Classified las
+# Plot classified las
 gnd <- filter_ground(las_classified)
-plot(gnd, size = 3, bg = "white")
+plot(gnd, size = 4, bg = "white")
 
+# Plot DTM
 dtm_tin_0 <- rasterize_terrain(las_classified, res = 1, algorithm = tin())
 plot_dtm3d(dtm_tin_0, bg = "white") 
 
+# Plot normalised las
 plot(las_normalised,bg = "white")
 
+# Plot canopy height model
 plot(las_chm, col = height.colors(50))
 plot(PlotTrees, add = TRUE, col = "red")
 
 
-
 # 8.1 Individual Tree Detection (ITD)
 
-MinimumTreeHeight <- 0.6
+MinimumTreeHeight <- 0.5
 
 # Local Maximum Filter with variable windows size
 # f <- function(z) {1 * z + 0.5}
@@ -69,7 +72,7 @@ plot(las_chm, col = height.colors(50))
 plot(sf::st_geometry(ttops), add = TRUE, pch = 3)
 
 # Tree detection results can also be visualized in 3D!
-x <- plot(las_normalised, bg = "white", size = 4)
+x <- plot(las_normalised, bg = "white", size = 2)
 add_treetops3d(x, ttops, radius = 0.15, fastTransparency = TRUE, alpha = 0.8)
 
 # Video
@@ -93,12 +96,12 @@ movie3d(
 # Testing csf cloth resolution = 1
 tic()
 las_classified_2 <- classify_ground(las, csf(sloop_smooth = TRUE, 
-                                             class_threshold = 0.01, 
-                                             cloth_resolution = 1,
+                                             class_threshold = 0.05, 
+                                             cloth_resolution = 0.5,
                                              rigidness = 1,
                                              time_step = 0.65))
 
-plot(las_classified_2, color = "Classification", size = 3, bg = "lightblue")
+plot(las_classified_2, color = "Classification", size = 3, bg = "white")
 print("CSF Ground filtering time:")
 toc()
 
@@ -107,7 +110,7 @@ gnd <- filter_ground(las_classified_2)
 plot(gnd, size = 3, bg = "white")
 
 # Classified ground
-dtm_tin_0 <- rasterize_terrain(las_classified_2, res = 1, algorithm = tin())
+dtm_tin_0 <- rasterize_terrain(las_classified_2, res = 0.01, algorithm = tin())
 plot_dtm3d(dtm_tin_0, bg = "white") 
 
 # Normalised 
